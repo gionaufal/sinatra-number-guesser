f8de55018c0b6ad15482e0c8156e5166a7a17e1e
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2017-07-17 17:06:40 -0300

    Adds color and cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,17 +6,25 @@ SECRET_NUMBER = rand(101)
 get '/' do
   guess = params['guess'].to_i
   message = guess_check(guess)
-  erb :index, locals: { number: SECRET_NUMBER, message: message }
+  display = cheat_mode(params['cheat'])
+  erb :index, locals: { number: SECRET_NUMBER, message: message, color: @@color, display: display }
 end
 
 def guess_check(guess)
   if guess == 0
     'Enter a guess'
   elsif SECRET_NUMBER == guess
+    @@color = 'green'
     'Correct!'
   elsif SECRET_NUMBER > guess
+    @@color = 'red'
     'The guess is too low!'
   elsif SECRET_NUMBER < guess
+    @@color = 'red'
     'The guess is too high!'
   end
+end
+
+def cheat_mode(cheat)
+  cheat ? '' : 'hidden'
 end
