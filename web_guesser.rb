5bf2ba80e0d702eacc0810000b067741dc388a59
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2017-07-18 15:00:29 -0300

    Add guess count

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,38 +1,58 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
+@@secret_number = rand(101)
+@@tries = 5
 
 get '/' do
   guess = params['guess'].to_i
   message = guess_check(guess)
   display = cheat_mode(params['cheat'])
-  erb :index, locals: { number: SECRET_NUMBER, message: message, color: @@color, display: display }
+  erb :index, locals: { number: @@secret_number, message: message, color: @@color, display: display, tries: @@tries }
 end
 
 def guess_check(guess)
-  diff = SECRET_NUMBER - guess
+  diff = @@secret_number - guess
   infinity = 1.0 / 0.0
 
-  case diff
-  when 0
-    @@color = 'green'
-    'Correct!'
-  when 1..25
-    @@color = 'orange'
-    'The guess is too low!'
-  when 26..infinity
-    @@color = 'red'
-    'The guess is way too low!!!'
-  when -infinity..-26
-    @@color = 'red'
-    'The guess is way too high!'
-  when -25..-1
-    @@color = 'orange'
-    'The guess is too high!'
+  unless guess.zero?
+    case diff
+    when 0
+      @@color = 'green'
+      @@tries = 5
+      'Correct! New number generated'
+    when 1..25
+      @@color = 'orange'
+      @@tries = @@tries - 1
+      guess_counter || 'The guess is too low!'
+    when 26..infinity
+      @@color = 'red'
+      @@tries = @@tries - 1
+      guess_counter || 'The guess is way too low!!!'
+    when -infinity..-26
+      @@color = 'red'
+      @@tries = @@tries - 1
+      guess_counter || 'The guess is way too high!'
+    when -25..-1
+      @@color = 'orange'
+      @@tries = @@tries - 1
+      guess_counter || 'The guess is too high!'
+    end
   end
 end
 
 def cheat_mode(cheat)
   cheat ? '' : 'hidden'
+end
+
+def guess_counter
+  if @@tries < 1
+    generate_secret_number
+    @@tries = 5
+    'You have no guesses left. New number generated'
+  end
+end
+
+def generate_secret_number
+  @@secret_number = rand(101)
 end
